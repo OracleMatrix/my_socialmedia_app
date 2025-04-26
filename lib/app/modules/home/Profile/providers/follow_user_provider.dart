5efa28a9a91977520beab2dd88fdd58be72bcfcc
ehsanmp <ehsanmohamadipoor@gmail.com>
2025-04-26 12:00:59 +0330

    added follow provider for API call

---
+++ lib/app/modules/home/Profile/providers/follow_user_provider.dart
@@ -0,0 +1,32 @@
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:my_socialmedia_app/app/data/Constants/constants.dart';
+
+class FollowUserProvider extends GetConnect {
+  Future followUser(Map<String, dynamic> data) async {
+    try {
+      final response = await post(
+        '${Constants.baseUrl}/api/follow',
+        data,
+        headers: {'auth': GetStorage().read(Constants.tokenKey)},
+      );
+      if (response.body != null) {
+        if (response.status.isOk) {
+          return response.body;
+        } else if (response.status.isServerError) {
+          throw 'Server is not reachable\nPlease try again later';
+        } else if (response.status.isNotFound) {
+          throw 'Not Found!';
+        } else if (response.status.isForbidden) {
+          throw 'Bad request\nCheck data and try again...';
+        } else {
+          throw response.body['message'] ?? response.bodyString;
+        }
+      } else {
+        throw 'Something went wrong!\nPlease try again later';
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
