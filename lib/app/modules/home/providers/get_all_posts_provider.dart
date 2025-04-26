62ca36abac0258b9f991a69f5d61de279bbbe0e5
ehsanmp <ehsanmohamadipoor@gmail.com>
2025-04-26 09:56:14 +0330

    model name changed

--- lib/app/modules/home/providers/get_all_posts_provider.dart
+++ lib/app/modules/home/providers/get_all_posts_provider.dart
@@ -4,15 +4,15 @@ import 'package:my_socialmedia_app/app/data/Constants/constants.dart';
 import 'package:my_socialmedia_app/app/modules/home/Models/get_all_posts_model.dart';
 
 class GetAllPostsProvider extends GetConnect {
-  Future<GetAllPosts?> getAllPosts() async {
+  Future<GetAllPostsModel?> getAllPosts() async {
     try {
       final response = await get(
-        '${Constants.baseUrl}/api/posts/',
+        '${Constants.baseUrl}/api/posts/${GetStorage().read(Constants.idKey)}',
         headers: {'auth': await GetStorage().read(Constants.tokenKey)},
       );
       if (response.body != null) {
         if (response.status.isOk) {
-          return getAllPostsFromJson(response.bodyString!);
+          return getAllPostsModelFromJson(response.bodyString!);
         } else if (response.status.isServerError) {
           throw 'Server is not reachable\nPlease try again later';
         } else if (response.status.isNotFound) {
